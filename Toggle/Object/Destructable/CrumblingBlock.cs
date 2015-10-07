dc10752a914867d37bfadd8dad9bb2288b307d9c
Merle <mtferguson@smcm.edu>
2015-10-07 15:48:23 -0400

    Working on crumblingblock

---
+++ Toggle/Object/Destructable/CrumblingBlock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+namespace Toggle
+{
+    class CrumblingBlock : Destructable
+    {
+        public CrumblingBlock(int xLocation, int yLocation, bool initialState) : base(xLocation, yLocation, initialState)
+        {
+            goodGraphic = Textures.textures["crumblingblock"];
+            badGraphic = Textures.textures["crumblingblock"];
+            width = 32;
+            height = 32;
+            imageBoundingRectangle = new Rectangle(0, 0, width, height);
+            hitBox = new Rectangle(xLocation, yLocation, width, height);
+        }
+
+
+
+    }
+}
